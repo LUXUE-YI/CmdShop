3ad84fd74e1c33b000ea07a1876ea9727f1648c9
LUXUE-YI <2271783423@qq.com>
2020-12-28 12:29:27 +0800

    1、修改密码和电话号码读取格式问题 2、改文件路径格式 3、登录失败时让用户重新登录

--- src/ReadExcel.java
+++ src/ReadExcel.java
@@ -7,45 +7,62 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.text.DecimalFormat;
 
 public class ReadExcel {
     /*
     readExcel是什么方法？成员方法
+    readExcel的返回值是一个用户类型的一维数组，如String[]，数组中存储一个个字符串，而Users[]存储一个个用户，用户有自己的名称、密码、地址、电话
+    readExcel的的形参是File类型的文件，调用方法时实参为一个文件类型的文件，才能解析出对应的文件内容
      */
-    public User[] readExcel(File file) {
-        User users[] = null;
+    public User[] readExcel(InputStream in) {
+        User users[] = null;  //定义一个user类型的一维数组users，其中存储一个个用户，初值为空，无用户
         try {
-            XSSFWorkbook xw = new XSSFWorkbook(new FileInputStream(file));
-            XSSFSheet xs = xw.getSheetAt(0);
-            users = new User[xs.getLastRowNum()];
+            XSSFWorkbook xw = new XSSFWorkbook(in);  //创建XSSFWorkbook类的对象xw
+            XSSFSheet xs = xw.getSheetAt(0);  //xw对象调用getSheetAt()方法，并赋值给xs，以获取Excel文件中的第一个表格（一个文件中有多个表格）
+            users = new User[xs.getLastRowNum()]; //xs.getLastRowNum()获取表格中的存有信息的总行数，并给users数组分配空间，相当于users = new User[3]
+
+            //从表格的第二行（因为第一行是表头标题，没有真正存储用户信息）开始对存有用户信息的每一行进行遍历，获取每行的信息
             for (int j = 1; j <= xs.getLastRowNum(); j++) {
-                XSSFRow row = xs.getRow(j);
-                User user = new User();
+                XSSFRow row = xs.getRow(j); //调用方法获取表格对象xs的第二行，赋给XSSFRow类型的row
+                User user = new User();   //创建一个User类的对象，实例化
+
+                //从每一行的第一个单元格开始，获取该行每个单元格的信息
                 for (int k = 0; k <= row.getLastCellNum(); k++) {
-                    XSSFCell cell = row.getCell(k);
-                    if (cell == null)
+                    XSSFCell cell = row.getCell(k); //获取该行row中的单元格，赋值给cell
+                    if (cell == null)   //如果单元格为空，则跳出本次循环，执行下一次循环
                         continue;
-                    if (k == 0) {
+                    if (k == 0) {    //如果单元格不为空，并且是该行的第一个单元格，则调用方法获取这个单元格的值，并给用户的名称属性赋值
                         user.setUsername(this.getValue(cell));
-                    } else if (k == 1) {
+                    }
+                    else if (k == 1) {   //如果单元格不为空，并且是该行的第二个单元格，则调用方法获取这个单元格的值，并给用户的密码属性赋值
                         user.setPassword(this.getValue(cell));
-                    } else if (k == 2) {
+                    }
+                    else if (k == 2) {   //如果单元格不为空，并且是该行的第三个单元格，则调用方法获取这个单元格的值，并给用户的地址属性赋值
                         user.setAddress(this.getValue(cell));
-                    } else if (k == 3) {
+                    }
+                    else if (k == 3) {   //如果单元格不为空，并且是该行的第一个单元格，则调用方法获取这个单元格的值，并给用户的电话号码属性赋值
                         user.setPhone(this.getValue(cell));
                     }
-                    users[j-1]=user;
+                    users[j-1]=user;  //将已有信息的用户存储到users数组中，j从1开始，但是存储到数组时应该存到users[0],
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return users;
+        return users;  //将存有用户信息的数组返回
     }
 
+
+    /*
+    ReadExcel的另一个成员方法，用于获取单元格中的值
+    */
     private String getValue(XSSFCell cell) {
         String value;
         CellType type = cell.getCellTypeEnum();
+
+        DecimalFormat decimalFormat = new DecimalFormat("#");
 
         switch (type) {
             case STRING:
@@ -58,7 +75,9 @@ public class ReadExcel {
                 value = cell.getBooleanCellValue() + "";
                 break;
             case NUMERIC:
-                value = cell.getNumericCellValue() + "";
+                //value = cell.getNumericCellValue() + "";
+                //double类型的cell（如120.0）和一个空字符串相连，最终为转成字符串（如转成字符串120.0）
+                value = decimalFormat.format(cell.getNumericCellValue());
                 break;
             case FORMULA:
                 value = cell.getCellFormula();
--- src/Test.java
+++ src/Test.java
@@ -1,26 +1,35 @@
 import java.io.File;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) {
-        //预先获取读文件
-        File file = new File("F:\\idea\\自己的代码\\CmdShop\\src\\users.xlsx");
-        ReadExcel read = new ReadExcel();
-        User users[] = read.readExcel(file);
-        System.out.println(users[0].getUsername());
-        System.out.println(users[0].getPassword());
+    public static void main(String[] args) throws ClassNotFoundException {
+        boolean flag = true; //标志变量
+        while (flag) {
+            //预先获取读文件
+            //File file = new File("F:\\idea\\自己的代码\\CmdShop\\src\\users.xlsx"); //创建文件对象
+            InputStream in = Class.forName("Test").getResourceAsStream("/users.xlsx"); //直接使用输入流的方法可移植性更好  而不用FileInputStream的方法
+            ReadExcel read = new ReadExcel(); // 创建ReadExcel类的对象
+            User users[] = read.readExcel(in);  //read对象调用readExcel方法，读取文件，会返回一个用户类型的数组，并赋值给users数组
 
-        //获取用户名和密码
-        Scanner sc = new Scanner(System.in);
-        System.out.println("请输入用户名：");
-        String username = sc.next();
-        System.out.println("请输入密码：");
-        String password = sc.next();
 
-        //将用户输入的名字和密码与Excel中的作比对
-        for(User user:users){
-            if(username.equals(user.getUsername()) && password.equals(user.getPassword())){
-                System.out.println("登录成功");
+            //获取用户名和密码
+            Scanner sc = new Scanner(System.in);
+            System.out.println("请输入用户名：");
+            String username = sc.next();
+            System.out.println("请输入密码：");
+            String password = sc.next();
+
+            //将用户输入的名字和密码与Excel中的作比对
+            for (User user : users) {
+                if (username.equals(user.getUsername()) && password.equals(user.getPassword())) {
+                    System.out.println("登录成功");
+                    flag= false; //当登录成功时变量为假，不再执行while循环
+                    break;
+                } else {
+                    System.out.println("登录失败");
+                    break;
+                }
             }
         }
     }
