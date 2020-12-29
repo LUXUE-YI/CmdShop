3c3f1e40b7f712a857e94b40b05572bad64d0bb4
LUXUE-YI <2271783423@qq.com>
2020-12-29 21:12:28 +0800

    1、让用户输入商品ID 2、在表格中寻找该商品 3、若找到该商品就把商品加入购物车

--- src/ReadProductExcel.java
+++ src/ReadProductExcel.java
@@ -14,7 +14,7 @@ public class ReadProductExcel {
     readExcel的返回值是一个用户类型的一维数组，如String[]，数组中存储一个个字符串，而Users[]存储一个个用户，用户有自己的名称、密码、地址、电话
     readExcel的的形参是File类型的文件，调用方法时实参为一个文件类型的文件，才能解析出对应的文件内容
      */
-    public Product[] readExcel(InputStream in) {
+    public Product[] getAllProduct(InputStream in) {
         Product products[] = null;  //定义一个user类型的一维数组users，其中存储一个个用户，初值为空，无用户
         try {
             XSSFWorkbook xw = new XSSFWorkbook(in);  //创建XSSFWorkbook类的对象xw
@@ -52,7 +52,43 @@ public class ReadProductExcel {
         return products;  //将存有用户信息的数组返回
     }
 
+    public Product getProductByID(String id,InputStream in) {
+        try {
+            XSSFWorkbook xw = new XSSFWorkbook(in);  //创建XSSFWorkbook类的对象xw
+            XSSFSheet xs = xw.getSheetAt(0);  //xw对象调用getSheetAt()方法，并赋值给xs，以获取Excel文件中的第一个表格（一个文件中有多个表格）
 
+            //从表格的第二行（因为第一行是表头标题，没有真正存储用户信息）开始对存有用户信息的每一行进行遍历，获取每行的信息
+            for (int j = 1; j <= xs.getLastRowNum(); j++) {
+                XSSFRow row = xs.getRow(j); //调用方法获取表格对象xs的第二行，赋给XSSFRow类型的row
+                Product product = new Product();   //创建一个User类的对象，实例化
+
+                //从每一行的第一个单元格开始，获取该行每个单元格的信息
+                for (int k = 0; k <= row.getLastCellNum(); k++) {
+                    XSSFCell cell = row.getCell(k); //获取该行row中的单元格，赋值给cell
+                    if (cell == null)   //如果单元格为空，则跳出本次循环，执行下一次循环
+                        continue;
+                    if (k == 0) {    //如果单元格不为空，并且是该行的第一个单元格，则调用方法获取这个单元格的值，并给用户的名称属性赋值
+                        product.setPID(this.getValue(cell));
+                    }
+                    else if (k == 1) {   //如果单元格不为空，并且是该行的第二个单元格，则调用方法获取这个单元格的值，并给用户的密码属性赋值
+                        product.setPName(this.getValue(cell));
+                    }
+                    else if (k == 2) {   //如果单元格不为空，并且是该行的第三个单元格，则调用方法获取这个单元格的值，并给用户的地址属性赋值
+                        product.setPrice(this.getValue(cell));
+                    }
+                    else if (k == 3) {   //如果单元格不为空，并且是该行的第一个单元格，则调用方法获取这个单元格的值，并给用户的电话号码属性赋值
+                        product.setDescription(this.getValue(cell));
+                    }
+                }
+                if(id.equals(product.getPID())){
+                    return product;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;  //将存有用户信息的数组返回
+    }
     /*
     ReadExcel的另一个成员方法，用于获取单元格中的值
     */
--- src/Test.java
+++ src/Test.java
@@ -13,8 +13,10 @@ public class Test {
 
             InputStream in2 = Class.forName("Test").getResourceAsStream("/product.xlsx"); //直接使用输入流的方法可移植性更好  而不用FileInputStream的方法
             ReadProductExcel readProduct = new ReadProductExcel(); // 创建ReadExcel类的对象
-            Product products[] = readProduct.readExcel(in2);
+            Product products[] = readProduct.getAllProduct(in2);
 
+            int count=0;//加入购物车的商品数
+            Product productes[] = new Product[5];//购物车
 
             //获取用户名和密码
             Scanner sc = new Scanner(System.in);
@@ -27,7 +29,11 @@ public class Test {
             for (User user:users) {
                 if (username.equals(user.getUsername()) && password.equals(user.getPassword())) {
                     System.out.println("登录成功");
+                    flag= false; //当登录成功时变量为假，不再执行while循环
 
+                    /*
+                    输出所有的商品的信息
+                     */
                     for(Product product:products){
                         System.out.println("商品ID：" + product.getPID() +
                                            "   商品名称：" + product.getPName() +
@@ -35,7 +41,24 @@ public class Test {
                                            "   商品描述: " +product.getDescription()
                         );
                     }
-                    flag= false; //当登录成功时变量为假，不再执行while循环
+
+                    /*
+                    让用户输入商品的ID，并将商品加入购物车
+                     */
+                    System.out.println("请输入商品ID:");
+                    String id = sc.next();
+                    in2=null;
+                    in2=Class.forName("Test").getResourceAsStream("/product.xlsx");
+                    ReadProductExcel readProduct1 = new ReadProductExcel(); // 创建ReadExcel类的对象
+                    Product product= readProduct1.getProductByID(id,in2);//寻找商品id
+
+                    //在表格中找到该商品，将该商品加入购物车
+                    if(product!=null){
+                        System.out.println("已找到该商品！");
+                        productes[count++]=product;
+                        System.out.println(productes[0].getPID());
+                    }
+
                     break;
                 } else {
                     System.out.println("登录失败");
